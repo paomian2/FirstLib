4624701d8068985261c48cd0f269fc015f3e1623
zhaoxiaodong <xiaodong.zhao@jinbenteng.com>
2018-04-26 14:07:46 +0800

    firstcommit

---
+++ firstliblib/src/main/java/com/liblibfirst/CommonUtils.java
@@ -0,0 +1,16 @@
+package com.liblibfirst;
+
+/**
+ * @author Linxz
+ *         time:2018年04月26日 13:55.
+ */
+
+public class CommonUtils {
+
+    public boolean isEmpty(String str){
+        if (str==null || str.length()==0){
+            return true;
+        }
+        return false;
+    }
+}
